c1a42a915b649177c8fd19556d2fea5797230206
meweit <cuibang02ur@126.com>
2021-05-16 10:02:05 +0800

    项目配置创建＆数据库配置

--- manage.py
+++ manage.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information35'
+    SQLALCHEMY_TRACK_MODIFITIONS = False
+
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
     return 'index'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
