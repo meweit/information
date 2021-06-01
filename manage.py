1b20d240b5f5db8b88c2e7b430e3aff12e72fef1
meweit <cuibang02ur@126.com>
2021-06-01 20:58:21 +0800

    session保存指定位置&测试

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask,session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
+from flask_session import Session
 
 
 class Config(object):
+    # 配置Secret_key
+    SECRET_KEY = '3fxmHberuyFpzJkgZplyuFCpy7aLfhz0ytkzHtyHfQxw1a4rqv7432kz4h3WFQ/+'
     # 项目配置
     DEBUG = True
     # 数据库配置
@@ -13,6 +16,16 @@ class Config(object):
     # redis配置
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
+    # Session 保存配置
+    SESSION_TYPE = 'redis'
+    # 开启Session签名
+    SESSION_USE_SIGNER = True
+    # 指定Session保存的redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 开启过期
+    SESSION_PERMANENT = False
+    # 设置过期时间　单位：秒
+    PERMANENT_SESSION_LIFETIME = 86480*2
 
 
 app = Flask(__name__)
@@ -24,10 +37,13 @@ db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 开启CSRF 保护
 CSRFProtect(app)
+# 设置session保存指定位置
+Session(app)
 
 
 @app.route('/')
 def index():
+    session['name'] = 'hello python'
     return 'index'
 
 
